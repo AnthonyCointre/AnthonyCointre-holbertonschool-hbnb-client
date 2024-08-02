f56738d6cd176530c4bb187d295a3d12ee4ddbfe
AnthonyCointre <8732@holbertonstudents.com>
2024-08-02 11:44:07 +0200

    modify scripts, styles and html files to work with

--- mock-api/static/scripts/scripts.js
+++ mock-api/static/scripts/scripts.js
@@ -1,8 +1,3 @@
-/*
-  This is a SAMPLE FILE to get you started.
-  Please, follow the project instructions to complete the tasks.
-*/
-
 document.addEventListener('DOMContentLoaded', function () {
   // Load header
   fetch('header.html')
@@ -24,46 +19,4 @@ document.addEventListener('DOMContentLoaded', function () {
     .then((data) => {
       document.getElementById('add-review').innerHTML = data;
     });
-});
-
-// Login
-document.addEventListener('DOMContentLoaded', () => {
-  const loginForm = document.getElementById('login-form');
-  const errorMessage = document.getElementById('error-message');
-
-  if (loginForm) {
-    loginForm.addEventListener('submit', async (event) => {
-      event.preventDefault();
-      const email = document.getElementById('email').value;
-      const password = document.getElementById('password').value;
-      try {
-        const response = await loginUser(email, password);
-        if (response.ok) {
-          const data = await response.json();
-          document.cookie = `token=${data.access_token}; path=/`;
-          window.location.href = 'index.html';
-        } else {
-          const errorData = await response.json();
-          displayError(`Login failed: ${errorData.message}`);
-        }
-      } catch (error) {
-        displayError('An error occurred. Please try again.');
-      }
-    });
-  }
-
-  function displayError(message) {
-    errorMessage.textContent = message;
-    errorMessage.style.display = 'block';
-  }
-
-  async function loginUser(email, password) {
-    return await fetch('https://your-api-url/login', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({ email, password })
-    });
-  }
 });
